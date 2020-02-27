48b8bd05fe80a7de7d7c0d9ab43860944f2cd73e
kimleang <lykimleang456@gmail.com>
2020-02-27 11:36:46 +0700

    second new

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatefulWidget{
+class Homepage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return HomepageStart();
   }
-
 }
-class HomepageStart extends State<Homepage>{
+
+class HomepageStart extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return null;
+    return Scaffold(
+        body: CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          pinned: true,
+          snap: true,
+          floating: true,
+          expandedHeight: 160,
+          backgroundColor: Colors.blueAccent[300],
+          iconTheme: IconThemeData(color: Colors.white),
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text('welcome to derleng'),
+            centerTitle: true,
+            background: Image.asset(
+              'image/sea_kh.jpg',
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+        ),
+        SliverFillRemaining(
+          child: Center(child:Text('put the body content here')),
+        )
+      ],
+    ));
   }
-
 }
